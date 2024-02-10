0d33b2521dbf6f2c76779d76bb1998e56f04cbc1
im-strange <itsmestrange.dump@gmail.com>
2024-02-10 19:48:46 +0800

    update

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 
 import csv
+import sys
 from texttable import Texttable
 
 """
 [+] each task contains:
+    - id
     - category
     - task
-    - time
     - deadline
+    - status
 """
 
 class app:
@@ -22,13 +24,13 @@ class app:
         return current_list
 
     def add(self, task_list):
+        id = len(self.list) + 1
         current_tasks = self.read_list()
+
+        task_list.insert(0, id)
         current_tasks.insert(0, task_list)
 
     def remove(self):
-        pass
-
-    def cli(self):
         pass
 
     def display_table(self):
@@ -38,6 +40,45 @@ class app:
             table.add_row(row)
         print(table.draw())
 
+class cli:
+    def __init__(self, args):
+        self.args = args
+        self.app = app()
+
+    def display_help(self):
+        help_msg = [
+            "Usage: mytask [OPTIONS]\n",
+            "Options:",
+            "  -h, --help\t Display help",
+            "  -a, --add\t Add a new task",
+            "  -d, --drop\t Delete a task",
+            "  -e, --edit\t Edit a task"
+        ]
+        for row in help_msg:
+            print(row)
+
+    def match(self, list1, list2):
+        result = any(item in list1 for item in list2)
+        return result
+
+    def run(self):
+        if self.match(["-h", "--help"], self.args):
+            self.display_help()
+
+        elif self.match(["-a", "--add"], self.args):
+            print("you are adding a tasks")
+
+        elif self.match(["-d", "--drop"], self.args):
+            print("you are removing a task")
+
+        elif self.match(["-e", "--edit"], self.args):
+            print("you are editing a task")
+
+        elif self.match(["--view"], self.args):
+            self.app.display_table()
+
 if __name__ == "__main__":
+    args = sys.argv
     todo = app()
-    todo.display_table()
+    cli = cli(args)
+    cli.run()
